acf0f3d760f7f0ed0e5ddd5d12b12ac4de2033c6
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-10-16 18:40:35 +0300

    fixed tests py3.5 and py2.6
    Change-Id: I95953cf08b7438604aa5b754b76466cd16287ef0

---
+++ packetary/library/drivers/base.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import logging
+import os
+import six
+
+from packetary.library import driver
+
+
+logger = logging.getLogger(__package__)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class IndexWriter(object):
+    @abc.abstractmethod
+    def add(self, package):
+        """Adds package to index."""
+
+    @abc.abstractmethod
+    def flush(self):
+        """Persistent changes on disk."""
+
+
+class BaseRepoDriver(driver.RepoDriver):
+    def __init__(self, context, arch):
+        self.arch = arch
+        self.context = context
+
+    @abc.abstractmethod
+    def create_index_writer(self, destination):
+        """Creates the index"""
+
+    @abc.abstractmethod
+    def load_packages(self, url, consumer):
+        """Loads packages by url."""
+
+    @abc.abstractmethod
+    def get_package_path(self, package):
+        """Gets the destination for package.
+        :return: list of path components
+        :rtype: list
+        """
+
+    @abc.abstractmethod
+    def url_iterator(self, urls):
+        """Gets iterator over urls"""
+
+    def load(self, urls, consumer):
+        """See Repository.load"""
+        if not isinstance(urls, (list, tuple)):
+            urls = [urls]
+
+        with self.context.get_execution_scope() as scope:
+            for url in self.url_iterator(urls):
+                scope.execute(
+                    self.load_packages,
+                    url, consumer
+                )
+
+    def clone(self, producer, destination):
+        index_writer = self.create_index_writer(destination)
+
+        with self.context.get_execution_scope() as scope:
+            for package in producer:
+                scope.execute(self._replicate_package, package, destination)
+                index_writer.add(package)
+        index_writer.flush()
+
+    def _replicate_package(self, package, destination):
+        """Synchronises remote file to local fs."""
+        connections = self.context.connections
+        offset = 0
+        dst_path = os.path.join(destination, *self.get_package_path(package))
+        try:
+            stats = os.stat(dst_path)
+            if stats.st_size == package.size:
+                logger.info("file %s is same.", dst_path)
+                return
+
+            if stats.st_size < package.size:
+                offset = stats.st_size
+        except OSError as e:
+            if e.errno != 2:
+                raise
+
+        logger.info(
+            "download: %s - %s, offset: %d",
+            package.url, dst_path, offset
+        )
+        with connections.acquire() as connection:
+            connection.retrieve(package.url, dst_path, offset)
---
+++ packetary/library/drivers/yum_driver.py
@@ -0,0 +1,120 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import lxml.etree as etree
+import os
+import six
+import six.moves.urllib.parse as urlparse
+import subprocess
+
+from packetary.library.gzip_stream import GzipDecompress
+
+from .base import IndexWriter
+from .base import logger
+from .base import BaseRepoDriver
+from .yum_package import YumPackage
+
+
+_namespaces = {
+    "main": "http://linux.duke.edu/metadata/common",
+    "md": "http://linux.duke.edu/metadata/repo"
+}
+
+
+def _find_createrepo():
+    """Finds the createrepo executable"""
+    paths = os.environ['PATH'].split(os.pathsep)
+    createrepo = os.environ.get("CREATEREPO_PATH", "createrepo")
+    if not os.path.isfile(createrepo):
+        for p in paths:
+            f = os.path.join(p, createrepo)
+            if os.path.isfile(f):
+                return f
+        return None
+    else:
+        return createrepo
+
+
+createrepo = _find_createrepo()
+
+
+class YumIndexWriter(IndexWriter):
+    def __init__(self, context, destination):
+        self.context = context
+        self.destination = destination
+        self.repos = set()
+
+    def add(self, p):
+        self.repos.add(p.repo)
+
+    def flush(self):
+        if createrepo is None:
+            six.print_(
+                "Please install createrepo utility and run the following "
+                "commands manually:"
+            )
+            command = lambda x: six.print_("\t", subprocess.list2cmdline(x))
+            executable = "createrepo"
+        else:
+            command = subprocess.check_call
+            executable = createrepo
+
+        for repo in self.repos:
+            path = os.path.join(self.destination, *repo)
+            if os.path.exists(os.path.join(path, "repodata", "repomd.xml")):
+                cmd = [executable, path, "--update"]
+            else:
+                cmd = [executable, path]
+            command(cmd)
+
+
+class YumRepoDriver(BaseRepoDriver):
+    """Yum repositories implementation"""
+
+    def get_package_path(self, p):
+        return list(p.repo) + p.filename.split("/")
+
+    def create_index_writer(self, destination):
+        return YumIndexWriter(self.context, destination)
+
+    def url_iterator(self, urls):
+        for url in urls:
+            if url.endswith("/"):
+                url = url[:-1]
+            yield "/".join((url, self.arch, ""))
+
+    def load_packages(self, baseurl, consumer):
+        """Reads packages from metdata."""
+        repomd = baseurl + "repodata/repomd.xml"
+        logger.debug("repomd: %s", repomd)
+
+        nodes = None
+        with self.context.connections.acquire() as connection:
+            repomd_tree = etree.parse(connection.open_stream(repomd))
+
+            node = repomd_tree.find("./md:data[@type='primary']", _namespaces)
+            if node is None:
+                raise ValueError("malformed meta: %s" % repomd)
+            location = node.find("./md:location", _namespaces).attrib["href"]
+
+            stream = GzipDecompress(connection.open_stream(
+                urlparse.urljoin(baseurl, location)
+            ))
+            nodes = etree.parse(stream)
+
+        for pkg_tag in nodes.iterfind("./main:package", _namespaces):
+            consumer(YumPackage(
+                pkg_tag,
+                baseurl
+            ))
