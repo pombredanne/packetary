c5849c2980f8d25ce8d7373d8e7c427f63763af7
Bulat Gaifullin <gaifullinbf@gmail.com>
2015-10-28 15:51:45 +0300

    fixed setup.py license
    Change-Id: I70b646b7bbf1baad9f2e3011928ba0926d2eeb61

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#    Copyright 2015 Mirantis, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
-import os
 import setuptools
 
 # In python < 2.7.4, a lazy loading of package `pbr` will break
@@ -25,19 +23,6 @@ try:
 except ImportError:
     pass
 
-
-def find_requires():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    requirements = []
-    with open('{0}/requirements.txt'.format(dir_path), 'r') as reqs:
-        for line in reqs:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                requirements.append(line)
-    return requirements
-
-
 setuptools.setup(
     setup_requires=['pbr'],
-    install_requires=find_requires(),
     pbr=True)
